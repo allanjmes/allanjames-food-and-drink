1efd0b5655a80a1fd8c8990ce479f8b7a5a443b3
allanjmes <allanjames.regala@gmail.com>
2025-04-09 13:55:35 +0800

    Add vite config setup for hot reload

--- wp-content/themes/my-custom-theme/functions.php
+++ wp-content/themes/my-custom-theme/functions.php
@@ -22,3 +22,40 @@ function is_vite_running($host = 'localhost', $port = 5173) {
     }
     return false;
 }
+
+function my_custom_theme_scripts() {
+    if (is_vite_running()) {
+        // Inject Vite HMR client for hot reloads
+        add_action('wp_footer', function () {
+            // Vite HMR Client
+            echo '<script type="module" src="http://localhost:5173/@vite/client"></script>';
+            // Vite main script
+            echo '<script type="module" src="http://localhost:5173/js/index.js"></script>';
+        });
+    } else {
+        // Production build
+        wp_enqueue_script(
+            'theme-js',
+            get_template_directory_uri() . '/dist/bundle.js',
+            [],
+            filemtime(get_template_directory() . '/dist/bundle.js'),
+            true
+        );
+
+        // Optional: enqueue built CSS if available
+        $css_file = get_template_directory() . '/dist/bundle.css';
+        if (file_exists($css_file)) {
+            wp_enqueue_style(
+                'theme-css',
+                get_template_directory_uri() . '/dist/bundle.css',
+                [],
+                filemtime($css_file)
+            );
+        }
+    }
+}
+add_action('wp_enqueue_scripts', 'my_custom_theme_scripts');
+
+function asdf() {
+
+}
