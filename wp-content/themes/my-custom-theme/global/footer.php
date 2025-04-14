9535ffd1f79058bc2fb0646e584b593bfd19e89c
allanjmes <allanjames.regala@gmail.com>
2025-04-14 20:24:11 +0800

    Add header and footer globals

---
+++ wp-content/themes/my-custom-theme/global/footer.php
@@ -0,0 +1,4 @@
+<footer>
+    <p>Logos and images from <a target="_blank" href="www.freepik.com">freepik.com</a></p>
+    <p>&copy; <?php echo date('Y'); ?> My Custom Theme. All rights reserved.</p>
+</footer>
