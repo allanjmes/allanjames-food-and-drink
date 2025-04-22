40d9ae1fd143ed82cfc4a7c0e7dac285a5d20d29
allanjmes <allanjames.regala@gmail.com>
2025-04-22 10:06:43 +0800

    Add development page

--- wp-content/themes/my-custom-theme/views/pages/development-page.php
+++ wp-content/themes/my-custom-theme/views/pages/development-page.php
@@ -1,1 +1,71 @@
-<h1>Development Page</h1>
+<section class="py-5">
+    <div>
+        <h1 class="fw-bold text-center">Development</h1>
+    </div>
+    <div class="container my-lg-5 d-flex flex-column gap-3 gap-lg-5">
+        <div class="d-flex flex-column justify-content-center align-items-center">
+            <h2 class="fw-bold">Vite Integration</h2>
+            <p>For minified CSS and Javascript</p>
+            <div>
+                <img
+                    src="<?php echo get_template_directory_uri(); ?>/assets/development/vite-integration.png"
+                    height="auto"
+                    width="100%"
+                    style="max-width: 500px;"
+                    alt=""
+                >
+            </div>
+        </div>
+        <div class="d-flex flex-column justify-content-center align-items-center">
+            <h2 class="fw-bold">Custom Theme Colors</h2>
+            <p></p>
+            <div>
+                <img
+                    src="<?php echo get_template_directory_uri(); ?>/assets/development/custom-theme-colors.png"
+                    height="auto"
+                    width="100%"
+                    style="max-width: 500px;"
+                    alt=""
+                >
+            </div>
+        </div>
+        <div class="d-flex flex-column justify-content-center align-items-center">
+            <h2 class="fw-bold">Bootstrap Integration and Overrides</h2>
+            <p>For faster development and applied theme</p>
+            <div>
+                <img
+                    src="<?php echo get_template_directory_uri(); ?>/assets/development/main-scss-file.png"
+                    height="auto"
+                    width="100%"
+                    style="max-width: 500px;"
+                    alt=""
+                >
+            </div>
+            <br>
+            <p>For extending Bootstrap Classes</p>
+            <div class="d-flex flex-column gap-3">
+                <img
+                    src="<?php echo get_template_directory_uri(); ?>/assets/development/bootstrap-overrides-1.png"
+                    height="auto"
+                    width="100%"
+                    style="max-width: 500px;"
+                    alt=""
+                >
+                <img
+                    src="<?php echo get_template_directory_uri(); ?>/assets/development/bootstrap-overrides-2.png"
+                    height="auto"
+                    width="100%"
+                    style="max-width: 500px;"
+                    alt=""
+                >
+                <img
+                    src="<?php echo get_template_directory_uri(); ?>/assets/development/bootstrap-overrides-3.png"
+                    height="auto"
+                    width="100%"
+                    style="max-width: 500px;"
+                    alt=""
+                >
+            </div>
+        </div>
+    </div>
+</section>
