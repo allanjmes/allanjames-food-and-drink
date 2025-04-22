c940b82cf5798658e9e117c7033c3595e14b3da5
allanjmes <allanjames.regala@gmail.com>
2025-04-22 09:22:21 +0800

    Add web design page

---
+++ wp-content/themes/my-custom-theme/views/pages/web-design-page.php
@@ -0,0 +1,28 @@
+<section class="py-5">
+    <div class="container my-lg-5">
+        <div class="d-flex flex-column gap-3 gap-lg-5 text-center">
+            <a target="_blank" href="https://dribbble.com/rylicstudio">
+                <h1 class="fw-bold">Web design by <span class="text-accent">Rylic Studio</span></h1>
+            </a>
+            <a target="_blank" href="https://dribbble.com/shots/25638342-Bitebox-Healthy-Food-Subscription-website">
+                <h1 class="fw-bold">From <span class="text-accent">dribbble.com</span></h1>
+            </a>
+        </div>
+        <div class="my-5">
+            <img
+                src="<?php echo get_template_directory_uri(); ?>/assets/web-design/mockup.webp"
+                height="auto"
+                width="100%"
+                alt=""
+            >
+        </div>
+        <div>
+            <img
+                src="<?php echo get_template_directory_uri(); ?>/assets/web-design/full-length.webp"
+                height="auto"
+                width="100%"
+                alt=""
+            >
+        </div>
+    </div>
+</section>
