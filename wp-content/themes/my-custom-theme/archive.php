8a831345998600ce196d05cfe47350a0736d1fe4
allanjmes <allanjames.regala@gmail.com>
2025-04-22 18:44:57 +0800

    Fix rendering of recipe page

--- wp-content/themes/my-custom-theme/archive.php
+++ wp-content/themes/my-custom-theme/archive.php
@@ -2,11 +2,11 @@
 
 <!-- LOAD FILES DEPENDING ON ARCHIVE TYPE -->
 <?php
-    if (is_post_type_archive('recipe')) :
-        require_once get_template_directory() . '/views/pages/recipe-page.php';
-    else:
-        echo 'No Content from archive.php';
-    endif;
+    // if (is_post_type_archive('recipe')) :
+    //     require_once get_template_directory() . '/views/pages/recipe-page.php';
+    // else:
+    //     echo 'No Content from archive.php';
+    // endif;
 ?>
 
 <?php get_footer(); ?>
--- wp-content/themes/my-custom-theme/views/page.php
+++ wp-content/themes/my-custom-theme/views/page.php
@@ -1,18 +1,16 @@
 <?php
-/**
- * Pages that are not here but on archive.php:
- *      '/recipe' page => recipe.php
- * --end
- */
 ?>
 
 <!-- LOAD FILES DEPENDING ON THE PAGES -->
+ <!-- from page.php -->
 <!-- PAGE TEMPLATES -->
 <?php
     if ( is_page( 'home' ) ) :
         require locate_template( 'views/pages/home-page.php' );
     elseif ( is_page( 'sample-page' ) ) :
         require locate_template( 'views/pages/sample-page.php' );
+    elseif ( is_page( 'recipe' ) ) :
+        require locate_template( 'views/pages/recipe-page.php' );
     elseif ( is_page( 'development' ) ) :
         require locate_template( 'views/pages/development-page.php' );
     elseif ( is_page( 'web-design' ) ) :
