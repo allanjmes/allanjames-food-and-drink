2d24a2ce1847aeb9e79a71c26f7c116ab0d15bc0
allanjmes <allanjames.regala@gmail.com>
2025-05-03 12:09:13 +0800

    Update hero banner

--- wp-content/themes/my-custom-theme/views/pages/home-page.php
+++ wp-content/themes/my-custom-theme/views/pages/home-page.php
@@ -13,12 +13,43 @@
             <div class="h-100 d-flex flex-column justify-content-center align-items-center gap-lg-5" id="home-banner-img">
                 <div class="h-100" style="position: absolute; top: 0; left: 0;">
                     <div class="m-3 d-flex flex-column justify-content-around" style="height: 90%;">
-                        <div>
-                            asdf
+                        <div class="d-flex">
+                            <div class="" style="">
+                                <img
+                                    src="<?php echo get_template_directory_uri(); ?>/assets/home/home-profile-1.png"
+                                    height="45px"
+                                    width="45px"
+                                    style="border-radius: 50%;"
+                                    alt="">
+                            </div>
+                            <div class="" style="margin-left: -10px;">
+                                <img
+                                    src="<?php echo get_template_directory_uri(); ?>/assets/home/home-profile-2.png"
+                                    height="45px"
+                                    width="45px"
+                                    style="border-radius: 50%;"
+                                    alt="">
+                            </div>
+                            <div class="" style="margin-left: -10px;">
+                                <img
+                                    src="<?php echo get_template_directory_uri(); ?>/assets/home/home-profile-3.png"
+                                    height="45px"
+                                    width="45px"
+                                    style="border-radius: 50%;"
+                                    alt="">
+                            </div>
+                            <div class="" style="margin-left: -10px;">
+                                <div class="bg-secondary d-flex justify-content-center align-items-center" style="height: 45px; width: 45px; border-radius: 50%;">
+                                    <p class="text-primary p-1 fw-extrabold m-0">18K+</p>
+                                </div>
+                            </div>
                         </div>
-                        <h1 class="d-none d-xl-block display-1 fw-extrabold text-custom-dark-blue" style="width: 95%;">FUEL UP YOUR DAY WITH OUR PLATTER!</h1>
-                        <h1 class="d-none d-md-block d-xl-none fw-extrabold w-75 text-custom-dark-blue">FUEL UP YOUR DAY WITH OUR PLATTER!</h1>
-                        <h5 class="d-md-none fw-bold w-75 text-custom-dark-blue">FUEL UP YOUR DAY WITH OUR PLATTER!</h5>
+                        <h1 class="d-none d-xxl-block display-2 fw-extrabold text-custom-dark-blue" style="width: 80%;">FUEL UP YOUR DAY WITH OUR PLATTER!</h1>
+                        <h2 class="d-none d-xl-block d-xxl-none display-2 fw-extrabold text-custom-dark-blue" style="width: 95%;" aria-hidden="true">FUEL UP YOUR DAY WITH OUR PLATTER!</h2>
+                        <h2 class="d-none d-lg-block d-xl-none display-3 fw-extrabold text-custom-dark-blue" style="width: 95%;" aria-hidden="true">FUEL UP YOUR DAY WITH OUR PLATTER!</h2>
+                        <h1 class="d-none d-md-block d-lg-none display-1 fw-extrabold text-custom-dark-blue" style="width: 60%;" aria-hidden="true">FUEL UP YOUR DAY WITH OUR PLATTER!</h1>
+                        <h2 class="d-none d-sm-block d-md-none fw-bold display-1 fw-extrabold text-custom-dark-blue" style="width: 70%;" aria-hidden="true">FUEL UP YOUR DAY WITH OUR PLATTER!</h2>
+                        <h2 class="d-sm-none fw-bold display-4 fw-extrabold text-custom-dark-blue" aria-hidden="true">FUEL UP YOUR DAY WITH OUR PLATTER!</h2>
                         <div>
                             <button class="btn-accent text-secondary">
                                 Learn More
